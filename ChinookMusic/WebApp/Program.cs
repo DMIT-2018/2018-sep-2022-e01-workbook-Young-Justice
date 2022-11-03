91cc6ff25392bbdb4db01e0c146a7fc1be9065cb
Dhwanil Patel <77370310+Young-Justice@users.noreply.github.com>
2022-11-02 20:45:00 -0600

    Review of menu items such as img, brand icon, submenu, referencing pages; review of appsettings and connections; prep Program.cs for adding class libraries

--- ChinookMusic/WebApp/Program.cs
+++ ChinookMusic/WebApp/Program.cs
@@ -1,13 +1,36 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using WebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Supplied database connection due to the fact that
+// we created this web app to use Individual Accounts
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add another GetConnectionString to reference our database
+// connectionString
+var connectionStringChinook = builder.Configuration.GetConnectionString("ChinookDB");
+
+// Given for the DB connection to DefaultConnection string
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// Code the DBConnection to the application DB Context for Chinook
+// The implementation of he connect AND registration of the
+// ChinookSystem services will be done in the ChinookSystem
+// class library.
+// So to accomplish this task, we will be using an "extension" method
+// The extension method will extend the IServiceCollection Class.
+// The extension method requires a parameter options.UseSqlServer(XXX)
+// where XXX is the connection string variable
+// builder.Services.ChinookSystemBackendDependencies(options => options.UseSqlServer(connectionStringChinook));
+
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
